dcfca9c44136fec49676ce8049a2be3b712ff3c3
Sergey Borovikov <serg.there@gmail.com>
2023-02-26 19:39:20 +0000

    ~

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Camera, CameraHelper, Color, DirectionalLight, DoubleSide, Group, HemisphereLight, Mesh, MeshStandardMaterial, PCFSoftShadowMap, Vector2 } from "three"
+import { Camera, CameraHelper, DirectionalLight, DoubleSide, Group, HemisphereLight, Mesh, MeshStandardMaterial, Vector2 } from "three"
+import { MODEL_LENGTH, STATUS } from "const"
 import { camera, controls, progressLabel, renderer, scene } from "init"
-import { BG, MODEL_LENGTH, STATUS } from "const"
 import { onEasedPointerMove } from "utils"
 import { loadModel } from "./load-model"
 import { House, HouseInnerMesh } from "./types"
@@ -12,31 +12,27 @@ controls.object = cameraPivot
 cameraPivot.position.set(1, 3, 6)
 cameraPivot.add(camera)
 scene.add(cameraPivot)
+
 onEasedPointerMove((pointer) => {
   camera.position.x = -pointer.x
   camera.position.y = -pointer.y
   camera.lookAt(scene.position)
 }, 5)
 
-scene.background = new Color(BG)
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = PCFSoftShadowMap
 
-const ambient = new HemisphereLight(0x997755, 0x557799, 0.5)
-scene.add(ambient)
 
-const direct = new DirectionalLight(0xffffff, 0.75)
-{
-  direct.position.set(2, 3, 4)
-  direct.castShadow = true
-  direct.shadow.mapSize = new Vector2(1024, 1024).multiplyScalar(4)
-  const distance = direct.position.length() + cameraPivot.position.length()
-  direct.shadow.camera.far = distance + 4.5
-  direct.shadow.bias = -0.001
-}
-cameraPivot.add(direct)
+const ambientLight = new HemisphereLight(0x997755, 0x557799, 0.5)
+scene.add(ambientLight)
 
-// const directCamera = new CameraHelper(direct.shadow.camera)
+const directLight = new DirectionalLight(0xffffff, 0.75)
+directLight.position.set(2, 3, 4)
+directLight.castShadow = true
+directLight.shadow.mapSize = new Vector2(1024, 1024).multiplyScalar(4)
+directLight.shadow.camera.far = directLight.position.length() + cameraPivot.position.length() + 4.5
+directLight.shadow.bias = -0.001
+cameraPivot.add(directLight)
+
+// const directCamera = new CameraHelper(directLight.shadow.camera)
 // scene.add(directCamera)
 
 
--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,11 @@
-import { ACESFilmicToneMapping, PerspectiveCamera, Scene, sRGBEncoding, WebGLRenderer } from "three"
+import { ACESFilmicToneMapping, Color, PCFSoftShadowMap, PerspectiveCamera, Scene, sRGBEncoding, WebGLRenderer } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { BG } from "const"
 
 
 
 export const scene = new Scene()
+scene.background = new Color(BG)
 
 export const camera = new PerspectiveCamera(60, innerWidth / innerHeight, 0.1, 100)
 
@@ -12,6 +14,8 @@ renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2))
 renderer.outputEncoding = sRGBEncoding
 renderer.toneMapping = ACESFilmicToneMapping
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = PCFSoftShadowMap
 document.querySelector(".canvas-container")!.append(renderer.domElement)
 
 export const controls = new OrbitControls(camera, renderer.domElement)
