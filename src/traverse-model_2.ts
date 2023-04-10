cecedf9161146ba999d527e617f27da44b33dd63
Sergey Borovikov <foretoo@proton.me>
2023-04-11 00:57:18 +0100

    new model

---
+++ src/traverse-model_2.ts
@@ -0,0 +1,51 @@
+import { Group, Mesh, MeshStandardMaterial } from "three"
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader"
+import { House, HouseInnerMesh } from "./types"
+
+
+
+export const traverseModel = (
+  gltf: GLTF,
+) => {
+  const model = gltf.scene
+  const scale = Array(3).fill(0.075) as [number, number, number]
+  const group = new Group() as House
+
+  model.traverse((object) => {
+    if (!(object instanceof Mesh)) return
+
+    const clone = object.clone() as HouseInnerMesh
+    clone.geometry.scale(...scale)
+    clone.castShadow = /^base$/.test(clone.name) ? false : true
+    clone.receiveShadow = true
+    clone.frustumCulled = false
+
+    switch (clone.name) {
+      case "walls":
+        clone.material = new MeshStandardMaterial({ color: 0xdddddd })
+        break
+      case "doors":
+      case "base":
+      case "glass":
+        clone.material = new MeshStandardMaterial({ color: 0x888888 })
+        break
+      case "tree":
+      case "terrain":
+      case "lounger":
+      case "sidewalk":
+      case "vent":
+      case "pole":
+        clone.material = new MeshStandardMaterial({ color: 0x555555 })
+        break
+      case "borders":
+      case "metal":
+        clone.material = new MeshStandardMaterial({ color: 0x333333 })
+        break
+    }
+
+    group.add(clone)
+    group.position.y = -1
+  })
+
+  return group
+}
